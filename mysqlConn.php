2e91c4a5afc1b79c59c576223256df4e3e0d795b
root <ethand320@yahoo.com>
2013-11-18 11:32:35 -0600

    first commit

---
+++ mysqlConn.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+class mysqlConn {
+
+
+private $conn;
+private $hostname = '216.231.132.54';
+private $username = 'bar';
+private $password = 'drower4';
+private $db_name = 'bar_app_db';
+
+
+    public  function mysqlConn(){
+    	echo 'called';
+    
+      //  $conn = mysql_connect($hostname, $username, $password);
+        $conn = mysql_connect("216.231.132.54", "bar", "drower4");
+      if (!$conn) die ('Could not connect to database:'   . mysql_errno());    
+	mysql_select_db("bar_app_db");
+echo 'passed selectdb';
+
+   	$result =  mysql_query('SELECT * FROM Bar WHERE BarIndex=1');
+	
+	 $array = mysql_fetch_array($result);
+                echo $array[0];
+                
+
+	echo 'finished?';
+        
+   }
+    
+    function getBar($barIndex){
+	echo 'get bar called';
+        
+        //select whole row based on the index
+   	$result =  $mysql_query( "SELECT * FROM Bar");
+
+	echo 'didnt work';
+
+//	 $array =$mysqli_fetch_array($result);
+  //              echo $array[0];
+                
+    }
+   
+    
+}
+
+
+?>
