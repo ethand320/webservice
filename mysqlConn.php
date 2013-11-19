a71b832858ff5ef0917012a7c024ba994b7ddb63
ethand320 <ethand320@yahoo.com>
2013-11-19 00:09:16 -0600

    cleaning up

--- mysqlConn.php
+++ mysqlConn.php
@@ -50,7 +50,7 @@ private $db_name = 'bar_app_db';
     $barDic['Name'] = $array[0];
     $barDic['Style'] = $array[2];
     $barDic['Description'] = $array[3];
-   // $barDic['Ranking'] = $array[4];		
+    $barDic['Ranking'] = $array[4];		
     //$barDic['BarIndex'] = $array[5];	
 	//echo 'finished adding array';
 	
