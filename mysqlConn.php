45be1192dae520f2b062cd6b048612160be943bb
ethand320 <ethand320@yahoo.com>
2013-11-20 15:00:03 -0600

    adding index

--- mysqlConn.php
+++ mysqlConn.php
@@ -55,7 +55,9 @@ private $db_name = 'bar_app_db';
     $barDic['Style'] = $array[2];
     $barDic['Description'] = $array[3];
     $barDic['Ranking'] = $array[4];
-    //index 5 is bar index value
+    $barDic['BarIndex'] = $array[5];
+    
+       //index 5 is bar index value
     $barDic['ImageUrl'] = $array[6];
     
         
